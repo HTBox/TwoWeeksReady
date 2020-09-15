b629535391360601c59948fc6b0cc2c8dcea972c
David Paquette <david.paquette@usask.ca>
2020-09-14 20:52:39 -0600

    Get the right web endpoint for static site hosting

--- az/create-frontend-resources.sh
+++ az/create-frontend-resources.sh
@@ -18,15 +18,18 @@ az configure -d group=$ENV_PREFIX
 az configure -d location=$LOCATION
 az group create -n $ENV_PREFIX
 
-echo "Creating shared Storage Account ${ENV_PREFIX}storage"
+echo "Creating static site Storage Account ${ENV_PREFIX}storage"
 STORAGE_ACCOUNT=${ENV_PREFIX}storage
 az storage account create -n $STORAGE_ACCOUNT
+az storage blob service-properties update --account-name $STORAGE_ACCOUNT --static-website --404-document 404.html --index-document index.html
+STORAGE_WEB_ENDPOINT=$(az storage account show -n twrdavestorage --query secondaryEndpoints.web -o tsv)
+
+echo "Storage endpoint for static site hosting: $STORAGE_WEB_ENDPOINT"
 
 CDN_PROFILE_NAME=${ENV_PREFIX}
 echo "Creating CDN Profile $CDN_PROFILE_NAME"
-az storage blob service-properties update --account-name $STORAGE_ACCOUNT --static-website --404-document 404.html --index-document index.html
 az cdn profile create -n $CDN_PROFILE_NAME --sku Standard_Microsoft
-az cdn endpoint create -n $CDN_PROFILE_NAME --profile-name $CDN_PROFILE_NAME --origin $STORAGE_ACCOUNT.z21.web.core.windows.net --origin-host-header $STORAGE_ACCOUNT.z21.web.core.windows.net --enable-compression
+az cdn endpoint create -n $CDN_PROFILE_NAME --profile-name $CDN_PROFILE_NAME --origin $STORAGE_WEB_ENDPOINT --origin-host-header $STORAGE_WEB_ENDPOINT --enable-compression
 az cdn endpoint rule add -n $CDN_PROFILE_NAME --profile-name $CDN_PROFILE_NAME --rule-name enforcehttps --order 1 --action-name "UrlRedirect"  --redirect-type Found --redirect-protocol HTTPS --match-variable RequestScheme --operator Equal --match-value HTTP
 az cdn endpoint rule add -n $CDN_PROFILE_NAME --profile-name $CDN_PROFILE_NAME --rule-name sparewrite --order 2 --action-name "UrlRewrite" --source-pattern '/' --destination /index.html --preserve-unmatched-path false --match-variable UrlFileExtension --operator LessThan --match-value 1
 
