cd19ce1af21fd25e62d72285579c5ee5f1a3e93e
David Paquette <david.paquette@usask.ca>
2020-10-06 20:48:48 -0600

    Starting point for Azure Functiosn / CosmosDB based API

---
+++ api/FamilyMembers/FamilyMember.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace TwoWeeksReady.FamilyMembers
+{
+    public class FamilyMember 
+    {
+        public Guid Id {get; set;}
+        public string Name {get; set;}
+    }
+}
