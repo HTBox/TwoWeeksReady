77b8c1a9c3c31ffe292b28b78972a13c201e4d20
codingbandit <carey.payette@gmail.com>
2020-10-19 12:08:55 -0400

    missed the content of prepare.vue

---
+++ 2wr-app/src/views/prepare/prepare.vue
@@ -0,0 +1,13 @@
+<template>
+  <PrepareLanding></PrepareLanding>
+</template>
+
+<script>
+import PrepareLanding from '@/components/prepare/prepare-landing.vue'
+export default {
+  name: 'Prepare',
+  components: {
+    PrepareLanding
+  }
+}
+</script>
