02a6cfd382cf634087bcf50cd84faec0c7110ccd
Tyler Doerksen <tylerd@microsoft.com>
2021-10-12 14:49:53 -0500

    Adding initial welcome view and landing components

---
+++ 2wr-app/src/views/welcome/welcome.vue
@@ -0,0 +1,13 @@
+<template>
+  <WelcomeLanding></WelcomeLanding>
+</template>
+
+<script>
+import WelcomeLanding from '@/components/welcome/welcome-landing.vue'
+export default {
+  name: 'Welcome',
+  components: {
+    WelcomeLanding
+  }
+}
+</script>
