c6a1994c269a95b34d2d4043eb69132a24ea06c3
rtmsft <92400369+rtmsft@users.noreply.github.com>
2021-10-13 16:47:05 -0700

    Added support for out of area contacts.

---
+++ 2wr-app/src/views/prepare/family-plans/distant-contacts.vue
@@ -0,0 +1,13 @@
+<template>
+  <FamilyPlansContacts :planId="planId" :distant=true></FamilyPlansContacts>
+</template>
+
+<script>
+import FamilyPlansContacts from "@/components/prepare/family-plans/family-plan-contacts-view.vue";
+import { defineComponent } from "@vue/composition-api";
+
+export default defineComponent({
+  components: { FamilyPlansContacts },
+  props: [ "planId" ]
+});
+</script>
