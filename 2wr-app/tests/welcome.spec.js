bf63270739d8282943b0c10bbe0026a377b36fb0
Tyler Doerksen <tylerd@microsoft.com>
2021-10-14 12:56:30 -0500

    adding accessibility test to welcome page

--- 2wr-app/tests/welcome.spec.js
+++ 2wr-app/tests/welcome.spec.js
@@ -1,7 +1,29 @@
-const { text, expect, default: test } = require('@playwright/test');
+const { text, expect, default: test, chromium } = require('@playwright/test');
+const { injectAxe, checkA11y } = require("axe-playwright");
 
-test('Welcome page loads', async({ page }) => {
-   
+test.describe('Welcome page accessibility test', () => {
+    test.beforeAll(async () => {
+        browser = await chromium.launch();
+        page = await browser.newPage();
+        await page.goto('/welcome/');
+        await injectAxe(page);
+    });
+
+    test('Check Accessability', async () => {
+        await checkA11y(page, null, {
+            axeOptions: {
+
+            },
+            detailedReport: true,
+            detailedReportOptions: {
+                html: true
+            }
+        });
+    });
+});
+
+test('Welcome page loads', async ({ page }) => {
+
     await page.goto('/');
 
     await expect(page).toHaveURL(/.*welcome/);
