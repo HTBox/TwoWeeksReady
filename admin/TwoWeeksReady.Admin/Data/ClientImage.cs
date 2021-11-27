a12d8bba03061554212f8d2256223dc55562c925
David Paquette <dave.paquette@outlook.com>
2021-11-27 09:00:11 -0700

    Ability to configure client images in admin app

---
+++ admin/TwoWeeksReady.Admin/Data/ClientImage.cs
@@ -0,0 +1,8 @@
+﻿namespace TwoWeeksReady.Admin.Data
+{
+    public class ClientImage
+    {
+        public string RelativePath { get; set; }
+        public string AbsolutePath { get; set; }
+    }
+}
