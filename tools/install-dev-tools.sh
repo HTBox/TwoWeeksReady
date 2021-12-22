c97539d1191fb195b9cb4bde0a1c52d64aa9ca8e
David Wesst <david@cocobokostudios.com>
2021-12-22 04:28:27 +0000

    added node, docker, and dotnet-install sdk scripts. #124

--- tools/install-dev-tools.sh
+++ tools/install-dev-tools.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+# restore and build project
+# dotnet restore ./api/api.sln
+
 # install nvs
-export NVS_HOME="$HOME/.nvs"
-git clone https://github.com/jasongin/nvs "$NVS_HOME"
-. "$NVS_HOME/nvs.sh" install
-nvs add lts
-nvs link lts
+# export NVS_HOME="$HOME/.nvs"
+# git clone https://github.com/jasongin/nvs "$NVS_HOME"
+# . "$NVS_HOME/nvs.sh" install
+# nvs add lts
+# nvs link lts
