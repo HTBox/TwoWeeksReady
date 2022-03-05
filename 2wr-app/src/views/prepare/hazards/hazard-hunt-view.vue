1c53c1589babfd9b433e11bcf6c1277cf8a9d807
codingbandit <carey.payette@gmail.com>
2022-03-05 13:58:58 -0500

    Adding Hazard Hunt information into the UI (#144)

---
+++ 2wr-app/src/views/prepare/hazards/hazard-hunt-view.vue
@@ -0,0 +1,14 @@
+<template>
+  <HazardHuntComponent></HazardHuntComponent>
+</template>
+
+<script>
+import HazardHuntComponent from '@/components/prepare/hazards/hazard-hunt.vue'
+
+export default {
+  name: 'HazardHunt',
+  components: {
+    HazardHuntComponent
+  }
+}
+</script>
