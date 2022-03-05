d998d91dde1234831e6c68ff6c301079f580d1d0
codingbandit <carey.payette@gmail.com>
2022-03-05 15:07:50 -0500

    Added a placeholder for the "Make A Plan" screen. This screen has a button to move to the family plan

---
+++ 2wr-app/src/views/prepare/make-a-plan/make-a-plan.vue
@@ -0,0 +1,14 @@
+<template>
+  <MakeAPlanComponent></MakeAPlanComponent>
+</template>
+
+<script>
+import MakeAPlanComponent from '@/components/prepare/make-a-plan/make-a-plan.vue'
+
+export default {
+  name: 'MakeAPlan',
+  components: {
+    MakeAPlanComponent
+  }
+}
+</script>
