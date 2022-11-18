8c976a3e4eb2d98c5460f0c4c691e8aefbf0d72b
David Paquette <dpaquette@microsoft.com>
2022-11-17 19:46:31 -0700

    Update Playwright version

--- 2wr-app/tests/welcome.spec.js
+++ 2wr-app/tests/welcome.spec.js
@@ -1,31 +1,30 @@
-const { text, expect, default: test, chromium } = require('@playwright/test');
+const { expect, test, chromium } = require("@playwright/test");
 const { injectAxe, checkA11y } = require("axe-playwright");
 
-test.describe('Welcome page accessibility test', () => {
-    test.beforeAll(async () => {
-        browser = await chromium.launch();
-        page = await browser.newPage();
-        await page.goto('/welcome/');
-        await injectAxe(page);
-    });
+test.describe("Welcome page accessibility test", () => {
+  let browser;
+  let page;
 
-    test('Check Accessability', async () => {
-        await checkA11y(page, null, {
-            axeOptions: {
+  test.beforeAll(async () => {
+    browser = await chromium.launch();
+    page = await browser.newPage();
+    await page.goto("/welcome/");
+    await injectAxe(page);
+  });
 
-            },
-            detailedReport: true,
-            detailedReportOptions: {
-                html: true
-            }
-        });
+  test("Check Accessability", async () => {
+    await checkA11y(page, null, {
+      axeOptions: {},
+      detailedReport: true,
+      detailedReportOptions: {
+        html: true
+      }
     });
+  });
 });
 
-test('Welcome page loads', async ({ page }) => {
+test("Welcome page loads", async ({ page }) => {
+  await page.goto("/");
 
-    await page.goto('/');
-
-    await expect(page).toHaveURL(/.*welcome/);
-
+  await expect(page).toHaveURL(/.*welcome/);
 });
